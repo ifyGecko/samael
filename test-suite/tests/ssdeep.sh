613a6c85064e9334fbe7e6c46f68dc582c4fe281
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2020-06-21 01:38:17 -0500

    corrected && shorted ssdeep test

--- test-suite/tests/ssdeep.sh
+++ test-suite/tests/ssdeep.sh
@@ -10,20 +10,16 @@ cd ../
 for i in {1..15}
 do
     make clean > /dev/null; make obfuscated > /dev/null
-    ssdeep src/samael > /tmp/samael_hashes.txt
-    ssdeep payloads/reverse_shell.so > /tmp/reverse_shell_hashes.txt
-    ssdeep payloads/bind_shell.so > /tmp/bind_shell_hashes.txt
+    ssdeep src/samael >> /tmp/hashes.txt
 done
 
-samael_result=$(make obfuscated > /dev/null && ssdeep -m /tmp/samael_hashes.txt src/samael)
-reverse_shell__result=$(make obfuscated > /dev/null && ssdeep -m /tmp/bind_shell_hashes.txt payloads/bind_shell.so)
-bind_shell__result=$(make obfuscated > /dev/null && ssdeep -m /tmp/reverse_shell_hashes.txt payloads/reverse_shell.so)
+samael_result=$(make obfuscated > /dev/null && ssdeep -s -m /tmp/hashes.txt src/samael)
 
-if [ "$samael_result" != "" ] || [ "$reverse_shell_result" != "" ] || [ "$bind_shell_result" != "" ]
+if [ "$samael_result" != "" ]
 then
     echo "[X] SSDEEP Test Failed"
 else
     echo "[*] SSDEEP Test Passed"
 fi
 
-rm /tmp/*_hashes.txt
+rm /tmp/hashes.txt
