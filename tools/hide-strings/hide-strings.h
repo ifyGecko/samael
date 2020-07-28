042570f360e1d6fed92f43f5c7ab0ace6f704658
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2020-07-28 16:19:42 -0500

    hide strings progress made, still WIP

--- tools/hide-strings/hide-strings.h
+++ tools/hide-strings/hide-strings.h
@@ -1,50 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-#define fwd_head_mv '>'
-#define bck_head_mv '<'
-#define inc_at_head '+'
-#define dec_at_head '-'
-#define print_at_head '.'
-#define start_loop '['
-#define end_loop ']'
+#include <string.h>
 
 char* head;
+char* start;
 
-void ebfin_init(unsigned int tape_size){
-  head = (char*)calloc(tape_size, sizeof(char));
+void __attribute__((constructor)) bf_init(){
+  head = (char*)malloc(0xFF * sizeof(char));
+  start = head;
 }
 
-void ebfin_halt(){
-  free(head);
-}
-
-void ebfin_eval(char* c){
-  for(int i = 0 ; c[i] != '\0' ; ++i){
-    if(c[i] == fwd_head_mv) ++head;
-    else if(c[i] == bck_head_mv) --head;
-    else if(c[i] == inc_at_head) ++*head;
-    else if(c[i] == dec_at_head) --*head;
-    else if(c[i] == print_at_head) putchar(*head);
-    else if(c[i] == start_loop){
-      if(*head == 0){
-	int j = 1;
-	while(j > 0){
-	  i++;
-	  if(c[i] == '[') j++;
-	  if(c[i] == ']') j--;
-	}
-      }
-    }
-    else if(c[i] == end_loop){
-      if(*head != 0){
-	int j = 1;
-	while(j > 0){
-	  i--;
-	  if(c[i] == '[') j--;
-	  if(c[i] == ']') j++;
-	}
-      }
-    }
+static inline __attribute__((always_inline)) char* bf_eval(char* c){
+  head = start;
+  memset(head, 0, 0xFF);
+  for(int i = 0 ; c[i] != '0' ; ++i){
+    if(c[i] == '>') ++head;
+    else if(c[i] == '<') --head;
+    else if(c[i] == '+') ++*head;
+    else if(c[i] == '-') --*head;
   }
+  return start;
 }
--- tools/hide-strings/hide-strings.sh
+++ tools/hide-strings/hide-strings.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-str=$1
-bf_str=''
+if [ ! -f "$1.bak" ]
+then
+    cat $1 > "$1.bak"
+fi
 
-for (( i=0; i<${#str}; i++ )); do
-    dec=$(echo "${str:$i:1}" | hexdump -v -e '/1 "%02d\n"' | head -n 1)
-    for (( j=0; j<dec; j++ )); do
-	bf_str+='+'
-    done
-    bf_str+='>'
-done
+rm $1
 
-echo $bf_str
+header=$(find / -name hide-strings.h 2> /dev/null)
+
+header='#include "'$header'"'
+
+echo $header > $1
+
+while read line; do
+    if [[ $line =~ "\"" ]] && [[ ! $line =~ "#include" ]]
+    then
+	str=$(echo $line | cut -d'"' -f 2)
+	bf_str=''
+	
+	for (( i=0; i<${#str}; i++ )); do
+	    dec=$(echo "${str:$i:1}" | hexdump -v -e '/1 "%02d\n"' | head -n 1)
+	    for (( j=0; j<dec; j++ )); do
+		bf_str+='+'
+	    done
+	    bf_str+='>'
+	done
+
+	str='"'$str'"'
+	bf_str='bf_eval("'$bf_str'")'
+	line=${line/$str/$bf_str}
+
+    fi
+    echo -e "${line}" >> $1
+done < "$1.bak"
