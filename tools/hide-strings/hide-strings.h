476cb3b9b1592136d7a885300d1a52f44a0695fc
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2020-08-02 01:08:56 -0500

    cleaned up header file

--- tools/hide-strings/hide-strings.h
+++ tools/hide-strings/hide-strings.h
@@ -1,17 +1,18 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-#define aaaa static inline __attribute__((always_inline))
-
-char* bf_eval(char* c){
+static inline __attribute__((always_inline)) char* bf_eval(char* c){
   char* head = (char*)calloc(0xFF, sizeof(char));
   char* start = head;
   for(int i = 0 ; c[i] != '\0' ; ++i){
-    if(c[i] == '>') ++head;
-    else if(c[i] == '<') --head;
-    else if(c[i] == '+') ++*head;
-    else if(c[i] == '-') --*head;
+    if(c[i] == '>'){
+      ++head;
+    }else if(c[i] == '<'){
+      --head;
+    }else if(c[i] == '+'){
+      ++*head;
+    }else if(c[i] == '-'){
+      --*head;
+    }
   }
   return start;
 }
