41076d72b906b5ef444c49bea030c20e93842dbe
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2021-01-16 19:44:08 -0600

    add gcc flags to reduce file size

--- src/build.sh
+++ src/build.sh
@@ -10,10 +10,10 @@ fi
 ../tools/obfuscator/obfuscator.sh samael.h
 ../tools/hide-strings/hide-strings.sh samael.c
 ../tools/hide-strings/hide-strings.sh samael.h
-gcc -Wl,--no-as-needed -ldl samael.c -o samael
+gcc -fno-ident -fno-asynchronous-unwind-tables -Wl,--no-as-needed -ldl samael.c -o samael
 ../tools/anti-debug/anti-debug.sh
 ./build.sh clean
-gcc -Wl,--no-as-needed -ldl samael.c -o samael
+gcc -fno-ident -fno-asynchronous-unwind-tables -Wl,--no-as-needed -ldl samael.c -o samael
 strip --strip-all samael
 r2 -2qc 'wox 0x03@0x5!1; q!!!' -w samael
 mv samael.c.bak samael.c
